ef7743cf8653e681be3a4c438df96ab4a93c6b22
frazer wilson <frazerwilson145@gmail.com>
2017-05-31 22:44:15 +0100

    style start

--- public/build/ball.js
+++ public/build/ball.js
@@ -53,7 +53,173 @@ function initMap() {
   var uluru = { lat: 51.541084, lng: -0.1048659 };
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    center: uluru
+    center: uluru,
+    styles: [{
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#1d2c4d"
+      }]
+    }, {
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#8ec3b9"
+      }]
+    }, {
+      "elementType": "labels.text.stroke",
+      "stylers": [{
+        "color": "#1a3646"
+      }]
+    }, {
+      "featureType": "administrative.country",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+        "color": "#4b6878"
+      }]
+    }, {
+      "featureType": "administrative.land_parcel",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#64779e"
+      }]
+    }, {
+      "featureType": "administrative.province",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+        "color": "#4b6878"
+      }]
+    }, {
+      "featureType": "landscape.man_made",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+        "color": "#334e87"
+      }]
+    }, {
+      "featureType": "landscape.natural",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#023e58"
+      }]
+    }, {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#283d6a"
+      }]
+    }, {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#6f9ba5"
+      }]
+    }, {
+      "featureType": "poi",
+      "elementType": "labels.text.stroke",
+      "stylers": [{
+        "color": "#1d2c4d"
+      }]
+    }, {
+      "featureType": "poi.business",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "poi.park",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "color": "#023e58"
+      }]
+    }, {
+      "featureType": "poi.park",
+      "elementType": "labels.text",
+      "stylers": [{
+        "visibility": "off"
+      }]
+    }, {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#3C7680"
+      }]
+    }, {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#304a7d"
+      }]
+    }, {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#98a5be"
+      }]
+    }, {
+      "featureType": "road",
+      "elementType": "labels.text.stroke",
+      "stylers": [{
+        "color": "#1d2c4d"
+      }]
+    }, {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#2c6675"
+      }]
+    }, {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [{
+        "color": "#255763"
+      }]
+    }, {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#b0d5ce"
+      }]
+    }, {
+      "featureType": "road.highway",
+      "elementType": "labels.text.stroke",
+      "stylers": [{
+        "color": "#023e58"
+      }]
+    }, {
+      "featureType": "transit",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#98a5be"
+      }]
+    }, {
+      "featureType": "transit",
+      "elementType": "labels.text.stroke",
+      "stylers": [{
+        "color": "#1d2c4d"
+      }]
+    }, {
+      "featureType": "transit.line",
+      "elementType": "geometry.fill",
+      "stylers": [{
+        "color": "#283d6a"
+      }]
+    }, {
+      "featureType": "transit.station",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#3a4762"
+      }]
+    }, {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [{
+        "color": "#0e1626"
+      }]
+    }, {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [{
+        "color": "#4e6d70"
+      }]
+    }],
+    disableDefaultUI: true
   });
   var gmarkers = [];
   // var marker = new google.maps.Marker({
@@ -81,12 +247,20 @@ function initMap() {
     });
     gmarkers.push(marker);
     var infowindow = new google.maps.InfoWindow({
-      content: 'ball is ' + numberWithCommas(ballDistance) + 'M from here'
+      content: 'You are <span>' + numberWithCommas(ballDistance) + 'M</span> from the ball'
+    });
+
+    google.maps.event.addListener(infowindow, 'domready', function () {
+      // Reference to the DIV which receives the contents of the infowindow using jQuery
+      var iwOuter = $('.gm-style-iw');
+      console.log(iwOuter.parentNode.childNodes[0].childNodes[3]);
+      iwOuter.parentNode.childNodes[0].childNodes[3].classList.add('style-window');
+      iwOuter.parentNode.childNodes[0].childNodes[2].childNodes[0].childNodes[0].classList.add('style-window');
+      iwOuter.parentNode.childNodes[0].childNodes[2].childNodes[1].childNodes[0].classList.add('style-window');
     });
 
     infowindow.open(map, marker);
   });
-  var contentString = '<div id="content">HERE</div>';
 
   addClass($("#map"), 'working'); //fetch the element with the id ‘someid’
 
@@ -121,21 +295,6 @@ function initMap() {
   };
 } // init map
 
-var x = document.getElementById("demo");
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-}
-
-getLocation();
 
 phonon.options({
   navigator: {
